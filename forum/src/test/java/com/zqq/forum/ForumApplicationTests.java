cc809d8e42a5e1896207f4c4e3afd50622c0148a
PwnDurant <1781389019@qq.com>
2025-03-09 23:23:18 +0800

    论坛系统

---
+++ forum/src/test/java/com/zqq/forum/ForumApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zqq.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("test:基于spring的论坛系统-前后端分离");
+    }
+
+}
