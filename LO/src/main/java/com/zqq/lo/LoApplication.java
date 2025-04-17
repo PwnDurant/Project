1dbe151b112f64f908f7e338caa5730e6de0d140
PwnDurant <1781389019@qq.com>
2025-04-17 15:48:40 +0800

    抽奖复写

---
+++ LO/src/main/java/com/zqq/lo/LoApplication.java
@@ -0,0 +1,13 @@
+package com.zqq.lo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoApplication.class, args);
+    }
+
+}
---
+++ LO/src/test/java/com/zqq/lo/LoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zqq.lo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
