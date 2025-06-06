d1966b720ad3119bfb5d0a791076806a8d129308
PwnDurant <1781389019@qq.com>
2025-06-06 21:20:04 +0800

    图书管理重构

---
+++ Blog_Improve/src/main/java/com/zqq/blog_improve/BlogImproveApplication.java
@@ -0,0 +1,13 @@
+package com.zqq.blog_improve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogImproveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogImproveApplication.class, args);
+    }
+
+}
---
+++ Blog_Improve/src/test/java/com/zqq/blog_improve/BlogImproveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zqq.blog_improve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogImproveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
