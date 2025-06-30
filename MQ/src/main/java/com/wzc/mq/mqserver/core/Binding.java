ef859753d6592c1f3dca5a3c31ce9dfc52573445
PwnDurant <1781389019@qq.com>
2025-06-30 14:30:53 +0800

    ChaseMQ

--- MQ/src/main/java/com/wzc/mq/mqserver/core/Binding.java
+++ MQ/src/main/java/com/wzc/mq/mqserver/core/Binding.java
@@ -1,7 +1,19 @@
 package com.wzc.mq.mqserver.core;
 
+import lombok.Data;
+
 /**
  * 表示一个队列和交换机之间的关联关系
  */
+@Data
 public class Binding {
+
+//    绑定的交换机的名称
+    private String exchangeName;
+
+//    绑定的消息队列名称
+    private String queueName;
+
+//    绑定之间的关系
+    private String bindingKey;
 }
--- MQ/src/main/java/com/wzc/mq/mqserver/core/MSGQueue.java
+++ MQ/src/main/java/com/wzc/mq/mqserver/core/MSGQueue.java
@@ -1,4 +1,29 @@
 package com.wzc.mq.mqserver.core;
 
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 消息队列
+ */
+@Data
 public class MSGQueue {
+
+//    标识队列的唯一身份标识
+    private String name;
+
+//    队列是否持久化
+    private boolean durable;
+
+//    这个属性为 true，表示这个队列只能被一个消费者所使用，如果为 false 表示大家都可以使用（后续拓展）
+    private boolean exclusive;
+
+//    如果该队列没有人使用的话就自动删除（后续拓展）
+    private boolean autoDelete = false;
+
+//    创建队列时指定的一些额外的选项（后续拓展）
+    private Map<String,Object> arguments =  new HashMap<>();
+
 }
